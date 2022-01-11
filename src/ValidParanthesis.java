6bd5883090121a8b432e45ef2c884fd17aa7dcae
divyabharathik <divya.bhaarathy@gmail.com>
2022-01-11 23:57:30 +0530

    Update ValidParanthesis.java

--- src/ValidParanthesis.java
+++ src/ValidParanthesis.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ValidParanthesis {
+public class ValidParanthesis {
+    public static void main(String[] args) {
+        System.out.println(isValid("[(]"));
+        System.out.println(isValid("{}"));
+        System.out.println(isValid("}("));
+
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> paran = new Stack<>();
+        if (s.length() > 1) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                    paran.push(s.charAt(i));
+                } else if ((paran.size() >= 1) && ((s.charAt(i) == ')' && paran.peek() == '(') || (s.charAt(i) == '}' && paran.peek() == '{') || (s.charAt(i) == ']' && paran.peek() == '['))) {
+                    paran.pop();
+                } else {
+                    return false;
+                }
+            }
+            return paran.empty();
+        } else {
+            return false;
+        }
+    }
 }
